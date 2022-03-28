fcbd8843afaee7aa0e28ff5e000482eb0afc7fdb
Hetrada <78165530+Hetrada@users.noreply.github.com>
2022-03-28 20:40:50 +0200

    ci4 MVC

---
+++ ci4/app/Views/templates/header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>CodeIgniter Tutorial</title>
+    <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url(); ?>/css/style.css" />
+    <script src="<?php echo base_url(); ?>/script/script.js"></script>
+</head>
+
+<body>
+    <?= base_url() ?>/public/style.css"
+    <h1><?= esc($title) ?></h1> 
