3bb4840c012d9f53ff4dc5263546e84c0e9e8ccf
Hetrada <Baptistelerate@gmail.com>
2022-03-28 09:32:30 +0200

    correction fichier php

--- Controler/CUserControler.php
+++ Controler/CUserControler.php
@@ -4,57 +4,6 @@
     require "CBaseControler.php";
     require "Model/MUser.php";
 
-    Class UsersController extends BaseController
-{
-    //Create View
-    public function create()
-    {
-        // It return add view in views/users/create.php
-        $this->render("add");
-    }
-
-    //Store action
-    public function store()
-    {
-        $result = User::store();
-
-        if ($result == $this->status_success)
-        {
-            $_SESSION["alert_success"] = $this->save_success;
-            echo "";
-        } 
-        else
-        {
-            $_SESSION["alert_danger"] = $this->save_error;
-            echo "";
-        }
-    }
-
-    //Edit View
-    public function edit()
-    {
-        // It return add view in views/users/edit.php
-        $user = User::find($id);
-        $data = ["user" => $user];
-        $this->render("edit", $data);
-    }
-
-    //Update Action
-    public function update()
-    {
-        $result = User::update();
-        
-        if ($result == $this->status_success)
-        {
-            $_SESSION["alert_success"] = $this->save_success;
-            echo "";
-        }
-        else
-        {
-            $_SESSION["alert_danger"] = $this->save_error;
-            echo "";
-        }
-    }
 
     //Delete Action
     public function delete()
