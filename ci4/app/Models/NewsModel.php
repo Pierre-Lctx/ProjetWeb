b377edf38000f492e8c07072e566ca0add5ea7c7
Hetrada <78165530+Hetrada@users.noreply.github.com>
2022-03-28 23:06:59 +0200

    appel procédure stocké codeigniter 4

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -8,6 +8,11 @@ class NewsModel extends Model
 {
     protected $table = 'address';
 
+    public function __construct()
+    {
+        $db=\Config\Database::connect();
+    }
+
     public function getAddress($slug = false)
     {
         if ($slug === false) {
@@ -16,5 +21,13 @@ class NewsModel extends Model
 
         return $this->where(['slug' => $slug])->first();
     }
+
+    public function searchIntership()
+    {
+        $sql = "CALL searchIntership()";
+        $query = $this->db->query("CALL `searchIntership`(offre, , , , , )");
+    }
+
+    
 }
     
